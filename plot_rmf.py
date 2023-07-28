038b29e0aa3413ba6fb42b481336332e10ba2a7c
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2023-07-27 21:29:55 -0400

    added --title option

--- plot_rmf.py
+++ plot_rmf.py
@@ -9,6 +9,7 @@ def main():
         description='Plot RMFs.'
     )
     parser.add_argument('-o', '--outfile', help='Save plot to named file.')
+    parser.add_argument('-t', '--title', help='Plot title.')
     parser.add_argument('files', nargs='+', help='RMF file(s).')
     args = parser.parse_args()
 
@@ -19,11 +20,9 @@ def main():
         plt.plot(0.5*(bin_lo+bin_hi), resp, label=label)
 
     plt.xlabel("λ (Å)")
-
-    if len(args.files)>1:
-        plt.legend()
-    else:
-        plt.title(label)
+    plt.legend()
+    if args.title:
+        plt.title(args.title)
 
     if args.outfile:
         plt.savefig(args.outfile)
