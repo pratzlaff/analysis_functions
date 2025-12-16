2f20dd19caeec482ac4e51bf5e046e458c0bf809
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2025-12-15 22:08:22 -0500

    Fixed typo in patch_hrc_ssc run.

--- util.bash
+++ util.bash
@@ -9,6 +9,6 @@ i_of_n() {
     unset args[1]
     args=( "${args[@]}" )
     argsstr=${args[@]}
-    subset=$(perl -I/data/legs/rpete/flight/ARLac/src -MARLac=split_array -le "\$i=$i; \$n=$n; @args=qw/$argsstr/;"'print join " ", @{(split_array($n, @args))[$i-1]};')
+    subset=$(perl -I/data/legs/rpete/flight/ARLac/src -MARLac=split_array -le "\$i=$i; \$n=$n; @args=qw!$argsstr!;"'print join " ", @{(split_array($n, @args))[$i-1]};')
     echo $subset
 }
